639ecf8921566f979096ec5a9d42b8f39c1de31a
Cherkaoui Jaafar <jaafarbuis55@gmail.com>
2024-08-08 21:43:44 +0100

    Added about page, plus added a Fixed layout

--- MedicalFront/src/app/app.component.ts
+++ MedicalFront/src/app/app.component.ts
@@ -1,25 +1,22 @@
 import { Component, Inject, PLATFORM_ID } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
-import { HeroComponent } from "./components/hero/hero.component";
-import { CardComponent } from "./components/card/card.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { TestimonialsComponent } from "./components/testimonials/testimonials.component";
 import { initFlowbite } from "flowbite";
 import { isPlatformBrowser } from "@angular/common";
 import { LayoutComponent } from "./components/layout/layout.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { FooterComponent } from "./components/footer/footer.component";
+
 
 @Component({
   selector: "app-root",
   standalone: true,
   imports: [
     RouterOutlet,
+    HomePageComponent,
     NavBarComponent,
-    HeroComponent,
-    CardComponent,
-    FooterComponent,
-    TestimonialsComponent,
-  ],
+    FooterComponent
+],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
