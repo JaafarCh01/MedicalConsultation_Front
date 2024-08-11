c4aa5db0ac595e30db3a3ef6b736ee40c3be9348
Cherkaoui Jaafar <jaafarbuis55@gmail.com>
2024-08-11 12:08:15 +0100

    Login , Register

--- MedicalFront/src/app/app.routes.ts
+++ MedicalFront/src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { AboutComponent } from "./pages/about/about.component";
 import { DocsComponent } from "./pages/docs/docs.component";
 import { ContactComponent } from "./pages/contact/contact.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { LoginComponent } from "./components/login/login.component";
 
 export const routes: Routes = [
     {path: '', component: HomePageComponent},
     {path: 'about', component: AboutComponent},
     {path: 'docs', component: DocsComponent},
-    {path: 'contact', component: ContactComponent}
+    {path: 'contact', component: ContactComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent}
 ];
 
 
