f589a33ce9fcb09fafa1eefd68184fa547bc1b70
Cherkaoui Jaafar <jaafarbuis55@gmail.com>
2024-08-11 11:09:58 +0100

    Search/Filer Bar

--- MedicalFront/tailwind.config.js
+++ MedicalFront/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
+      colors: {
+        'custom-blue': '#40A9FF',
+      },
       fontSize: {
         xxs: "0.625rem", // Example size for extra extra small text
       },
